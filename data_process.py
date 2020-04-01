05264de833925b17cb6b40f6aadc9125441dedb1
lzh <1652961256@qq.com>
2020-04-02 00:05:26 +0800

    count the entries

--- data_process.py
+++ data_process.py
@@ -1,12 +1,13 @@
 import os
+from itertools import islice
 
 data_path = '/home2/swp/data/twitter/'
 N = 0
-max_lines = 100000
+max_lines = 10000000
 with open(os.path.join(data_path, "training.tsv"), encoding="utf-8") as f:
 	while True:
-		lines = f.readlines(max_lines)
+		lines = list(islice(f, max_lines))
 		N += len(lines)
-		if not N:
+		if not lines:
 			break
 print(N)
