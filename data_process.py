5e3f512585104b637ba433b27e8d41be972f8146
Ziheng_Li <1652961256@qq.com>
2020-04-02 21:37:58 +0800

    untested data_process

--- data_process.py
+++ data_process.py
@@ -47,6 +47,9 @@ def data_count():
 bert = BertModel.from_pretrained('bert-base-multilingual-cased')
 
 def process(data):
+	statistic = np.load('statistic.npz')
+	all_language = statistic['language'][()]
+
 	tokens = data[features_to_idx['text_tokens']]
 	segments = [1] * len(tokens)
 	tokens = torch.tensor(tokens, device=device)
@@ -54,9 +57,57 @@ def process(data):
 	bert.eval()
 	with torch.no_grad():
 		layers, _ = bert(tokens, segments)
-	sentence_embedding = torch.mean(layers[11], 1)
+	sentence_embedding = torch.mean(layers[11], 1).cpu()
 
-	return sentence_embedding
+	media = torch.tensor([0., 0., 0.])
+	if 'Thoto' in data[features_to_idx['present_media']]:
+		media[0] = 1
+	if 'Video' in data[features_to_idx['present_media']]:
+		media[1] = 1
+	if 'Gif' in data[features_to_idx['present_media']]:
+		media[2] = 1
+
+	tweet_type = torch.tensor([0., 0., 0., 0.])
+	if "Retweet" == data[features_to_idx['tweet_type']]:
+		tweet_type[0] = 1
+	elif "Quote" == data[features_to_idx['tweet_type']]:
+		tweet_type[1] = 1
+	elif "Reply" == data[features_to_idx['tweet_type']]:
+		tweet_type[2] = 1
+	elif "Toplevel" == data[features_to_idx['tweet_type']]:
+		tweet_type[3] = 1
+
+	language = torch.zeros((len(all_language)), dtype=torch.float32)
+	language[all_language[features[features_to_idx['language']]]] = 1
+
+	LM_fer = np.log(statistic['M_fer'] + 1)
+	LM_fng = np.log(statistic['M_fng'] + 1)
+	normed_fer1 = torch.tensor([np.log(int(features[features_to_idx['engaged_with_user_follower_count']]) + 1) / LM_fer])
+	normed_fng1 = torch.tensor([np.log(int(features[features_to_idx['engaged_with_user_following_count']]) + 1) / LM_fng])
+	normed_fer2 = torch.tensor([np.log(int(features[features_to_idx['engaging_user_follower_count']]) + 1) / LM_fer])
+	normed_fng2 = torch.tensor([np.log(int(features[features_to_idx['engaging_user_following_count']]) + 1) / LM_fng])
+
+	engaged_verified = torch.tensor([0., 0.])
+	if features[features_to_idx['engaged_with_user_is_verified']]:
+		engaged_verified[1] = 1
+	else:
+		engaged_verified[0] = 1
+
+	engaging_verified = torch.tensor([0., 0.])
+	if features[features_to_idx['engaging_user_is_verified']]:
+		engaging_verified[1] = 1
+	else:
+		engaging_verified[0] = 1
+
+	follow = torch.tensor([0., 0.])
+	if features[features_to_idx['engagee_follows_engager']]:
+		follow[1] = 1
+	else:
+		follow[0] = 1
+
+	return [sentence_embedding, media, tweet_type, language, normed_fer1, normed_fng1, engaged_verified,
+			normed_fer2, normed_fng2, engaging_verified, follow,
+			bool(features[-4]), bool(features[-3]), bool(features[-2]), bool(features[-1])]
 
 with open(os.path.join(data_path, "training.tsv"), encoding="utf-8") as f:
 	lines = f.readlines(10)
