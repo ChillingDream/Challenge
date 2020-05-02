ff08dea7789d6b855d9accf0ad9f969d41e36f7c
Ziheng_Li <1652961256@qq.com>
2020-05-03 00:04:12 +0800

    add dnn to autoint

--- models/autoint.py
+++ models/autoint.py
@@ -42,13 +42,24 @@ class MutiheadAttention(nn.Module):
 		return outputs
 
 class AutoInt(nn.Module):
-	def __init__(self, emb_length, num_units, num_heads):
+	def __init__(self, emb_length, num_units, num_heads, dnn_units=[256, 128]):
 		super().__init__()
 		self.token_emb_layer = nn.EmbeddingBag.from_pretrained(word_embeddings, freeze=True, mode='mean')
 		self.emb_layers = nn.ModuleList([nn.Linear(word_embeddings.size()[1], emb_length, bias=False)] +
 										[nn.EmbeddingBag(field_dims[i], emb_length, mode='mean') for i in
 										 multihot_idx] +
 										[nn.Embedding(field_dims[i], emb_length) for i in onehot_idx])
+		self.dnn_layer = None
+		if dnn_units:
+			dnn = [Flatten()]
+			dnn_units = [len(field_dims) * emb_length] + dnn_units
+			for i in range(len(dnn_units) - 2):
+				dnn.append(nn.Linear(dnn_units[i], dnn_units[i + 1], bias=False))
+				dnn.append(nn.BatchNorm1d(dnn_units[i + 1]))
+				dnn.append(nn.ReLU(True))
+				dnn.append(nn.Dropout(drop_rate))
+			dnn.append(nn.Linear(dnn_units[-2], dnn_units[-1]))
+			self.dnn_layer = nn.Sequential(*dnn)
 		for layer in self.emb_layers[1:]:
 			nn.init.normal_(layer.weight, 0, emb_length ** -0.5)
 		layers = []
@@ -58,16 +69,21 @@ class AutoInt(nn.Module):
 			else:
 				layers.append(MutiheadAttention(num_units[i - 1] * num_heads[i - 1], num_units[i], num_heads[i]))
 		layers.append(Flatten())
-		layers.append(nn.Linear(len(field_dims) * num_units[-1] * num_heads[-1], 1, bias=True))
-		layers.append(nn.Sigmoid())
-		self.layers = nn.Sequential(*layers)
+		self.att_layers = nn.Sequential(*layers)
+		self.last_layer = nn.Linear(
+			len(field_dims) * num_units[-1] * num_heads[-1] + (dnn_units[-1] if dnn_units else 0),
+			1, bias=True)
 
 		self.loss_function = nn.BCELoss()
 
 	def forward(self, x):
 		x = [[self.token_emb_layer(*x[0])]] + x[1:]
 		emb = torch.stack([layer(*field) for (field, layer) in zip(x, self.emb_layers)], 1)
-		return self.layers(emb)
+		out = self.att_layers(emb)
+		if self.dnn_layer:
+			out = torch.cat([out, self.dnn_layer(emb)], 1)
+		out = self.last_layer(out)
+		return torch.sigmoid(out)
 
 	def loss(self, logits, labels):
 		return self.loss_function(logits, labels.float().to(device))
